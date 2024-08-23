562c22a66eb0248daf98161fe05bd987a523eba6
kerimcanbalkan <kerimcanbalkan@gmail.com>
2024-08-24 01:30:15 +0300

    add and list complete

--- cmd/add.go
+++ cmd/add.go
@@ -5,7 +5,6 @@ package cmd
 
 import (
 	"encoding/csv"
-	"errors"
 	"log"
 	"os"
 	"strconv"
@@ -45,37 +44,53 @@ func init() {
 }
 
 func add(cmd *cobra.Command, args []string) {
-	lastID := 1
-	file, err := os.OpenFile("output.csv", os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
+	// Open the file in append mode
+	file, err := os.OpenFile("output.csv", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Failed to open file:", err)
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Fatal("Failed to close file:", err)
+		}
+	}()
+
+	// Check if the file is empty to decide if we need to write headers
+	stat, err := file.Stat()
+	if err != nil {
+		log.Fatal("Failed to get file stats:", err)
 	}
 
+	// Create a new CSV writer
 	writer := csv.NewWriter(file)
 
-	if errors.Is(err, os.ErrNotExist) {
+	// If the file is empty, write headers
+	if stat.Size() == 0 {
 		headers := []string{"ID", "Description", "CreatedAt", "IsComplete"}
 		if err := writer.Write(headers); err != nil {
-			log.Fatal("Failed to write Headers", err)
+			log.Fatal("Failed to write headers:", err)
 		}
-
-	} else {
-		lines, err := csv.NewReader(file).ReadAll()
-		if err != nil {
-			log.Fatal("Couldn't Read the file")
-		}
-		lastID = len(lines)
+		writer.Flush()
 	}
 
-	defer file.Close()
+	// Read existing records to determine the next ID
+	file.Seek(0, 0) // Rewind to the beginning of the file to read all records
+	lines, err := csv.NewReader(file).ReadAll()
+	if err != nil {
+		log.Fatal("Failed to read file:", err)
+	}
 
+	lastID := len(lines)
+
+	// Create a new data entry
 	data := Data{
-		ID:          lastID,
+		ID:          lastID, // ID will be between 1 and 9
 		Description: args[0],
 		CreatedAt:   time.Now(),
 		IsComplete:  false,
 	}
 
+	// Write the new record to the CSV
 	record := []string{
 		strconv.FormatInt(int64(data.ID), 10),
 		data.Description,
@@ -84,13 +99,13 @@ func add(cmd *cobra.Command, args []string) {
 	}
 
 	if err := writer.Write(record); err != nil {
-		log.Fatal("Failed to write record", err)
+		log.Fatal("Failed to write record:", err)
 	}
 
+	// Ensure the data is flushed to the file
 	writer.Flush()
 
 	if err := writer.Error(); err != nil {
-		log.Fatal("Failed to flush writer", err)
+		log.Fatal("Failed to flush writer:", err)
 	}
-
 }
--- cmd/list.go
+++ cmd/list.go
@@ -1,12 +1,18 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"encoding/csv"
+	"errors"
 	"fmt"
+	"log"
+	"os"
+	"text/tabwriter"
+	"time"
 
+	"github.com/mergestat/timediff"
 	"github.com/spf13/cobra"
 )
 
@@ -20,16 +26,13 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
-	},
+	Run: list,
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
 
 	// Here you will define your flags and configuration settings.
-
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
 	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
@@ -37,4 +40,38 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func list(cmd *cobra.Command, args []string) {
+	file, err := os.Open("output.csv")
+	if errors.Is(err, os.ErrNotExist) {
+		fmt.Println("There are no tasks added!")
+		return
+	} else if err != nil {
+		log.Fatal("Couldn't open the file", err)
+	}
+
+	lines, err := csv.NewReader(file).ReadAll()
+	if err != nil {
+		log.Fatal("Couldn't read the csv")
+	}
+	writer := tabwriter.NewWriter(
+		os.Stdout, 0, 2, 4, ' ', 0,
+	)
+
+	writer.Write([]byte("ID\tDescription\tCreatedAt\tIsComplete\n"))
+	for _, line := range lines {
+		if line[0] == "ID" {
+			continue
+		}
+		parsedTime, err := time.Parse(time.RFC3339, line[2])
+		if err != nil {
+			log.Fatal("Failed to parse time:", err)
+		}
+		timeDifference := timediff.TimeDiff(parsedTime)
+		formattedString := fmt.Sprintf("%s\t%s\t%s\t%s\n", line[0], line[1], timeDifference, line[3])
+		writer.Write([]byte(formattedString))
+	}
+	writer.Flush()
+	defer file.Close()
 }
