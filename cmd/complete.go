b6fd665def993738239fdf9a41d622073e120317
kerimcanbalkan <kerimcanbalkan@gmail.com>
2024-08-24 20:23:36 +0300

    complete and delete done

--- cmd/complete.go
+++ cmd/complete.go
@@ -1,28 +1,23 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"encoding/csv"
+	"errors"
 	"fmt"
-
 	"github.com/spf13/cobra"
+	"log"
+	"os"
 )
 
 // completeCmd represents the complete command
 var completeCmd = &cobra.Command{
 	Use:   "complete",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("complete called")
-	},
+	Short: "This command marks given task as complete",
+	Long:  `Example usage tasks complete <task ID>`,
+	Run:   complete,
 }
 
 func init() {
@@ -37,4 +32,57 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// completeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func complete(cmd *cobra.Command, args []string) {
+	id := args[0]
+	var keepers [][]string
+	taskUpdated := false
+
+	file, err := os.Open("output.csv")
+	if errors.Is(err, os.ErrNotExist) {
+		fmt.Println("There are no tasks added!")
+		return
+	} else if err != nil {
+		log.Fatal("Couldn't open the file", err)
+	}
+
+	lines, err := csv.NewReader(file).ReadAll()
+	if err != nil {
+		log.Fatal("Couldn't read the csv")
+	}
+
+	for _, line := range lines {
+		if line[0] != id {
+			keepers = append(keepers, line)
+		} else {
+			updated := []string{line[0], line[1], line[2], "true"}
+			keepers = append(keepers, updated)
+			taskUpdated = true
+		}
+	}
+
+	if !taskUpdated {
+		fmt.Printf("Task with given id %s not found", id)
+		return
+	}
+
+	f, err := os.Create("output.csv")
+	if err != nil {
+		log.Fatal("Could not create the file", err)
+	}
+	writer := csv.NewWriter(f)
+
+	for _, el := range keepers {
+		if err := writer.Write(el); err != nil {
+			log.Fatal("Failed to write record:", err)
+		}
+
+	}
+
+	writer.Flush()
+	if err := writer.Error(); err != nil {
+		log.Fatal("Failed to flush writer:", err)
+	}
+
 }
--- cmd/delete.go
+++ cmd/delete.go
@@ -1,13 +1,15 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"encoding/csv"
+	"errors"
 	"fmt"
-
 	"github.com/spf13/cobra"
+	"log"
+	"os"
 )
 
 // deleteCmd represents the delete command
@@ -20,9 +22,7 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("delete called")
-	},
+	Run: delete,
 }
 
 func init() {
@@ -37,4 +37,55 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// deleteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func delete(cmd *cobra.Command, args []string) {
+	id := args[0]
+	var keepers [][]string
+	taskDeleted := false
+
+	file, err := os.Open("output.csv")
+	if errors.Is(err, os.ErrNotExist) {
+		fmt.Println("There are no tasks added!")
+		return
+	} else if err != nil {
+		log.Fatal("Couldn't open the file", err)
+	}
+
+	lines, err := csv.NewReader(file).ReadAll()
+	if err != nil {
+		log.Fatal("Couldn't read the csv")
+	}
+
+	for _, line := range lines {
+		if line[0] != id {
+			keepers = append(keepers, line)
+		} else {
+			taskDeleted = true
+		}
+	}
+
+	if !taskDeleted {
+		fmt.Printf("Task with given id %s not found", id)
+		return
+	}
+
+	f, err := os.Create("output.csv")
+	if err != nil {
+		log.Fatal("Could not create the file", err)
+	}
+	writer := csv.NewWriter(f)
+
+	for _, el := range keepers {
+		if err := writer.Write(el); err != nil {
+			log.Fatal("Failed to write record:", err)
+		}
+
+	}
+
+	writer.Flush()
+	if err := writer.Error(); err != nil {
+		log.Fatal("Failed to flush writer:", err)
+	}
+
 }
